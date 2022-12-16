de8106e2df8a8d003b861d5c493d7805ee2c9c61
Soufiane El marsi <sel-mars@e2r4p10.1337.ma>
2022-12-16 20:34:42 +0100

    ftp_wip

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: sel-mars <sel-mars@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/12/15 18:48:40 by sel-mars          #+#    #+#              #
-#    Updated: 2022/12/15 19:29:31 by sel-mars         ###   ########.fr        #
+#    Updated: 2022/12/16 20:34:19 by sel-mars         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,5 +20,21 @@ all: $(NAME)
 
 $(NAME): $(CMPS)
 	@mkdir -p $(VOLUMES)
+	@docker-compose --verbose -f $(COMPOSE) up --build
 
-.PHONY: all
+build:
+	@docker-compose --verbose -f $(COMPOSE) build
+
+up:
+	@docker-compose --verbose -f $(COMPOSE) up
+
+clean:
+	@docker-compose --verbose -f $(COMPOSE) stop
+
+fclean:
+	@docker-compose --verbose -f $(COMPOSE) down
+	@rm -rf volumes
+
+re: fclean all
+
+.PHONY: all clean fclean
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,7 +6,7 @@
 #    By: sel-mars <sel-mars@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/12/15 18:33:51 by sel-mars          #+#    #+#              #
-#    Updated: 2022/12/15 19:58:18 by sel-mars         ###   ########.fr        #
+#    Updated: 2022/12/16 15:44:06 by sel-mars         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,6 +22,10 @@ cd /var/www/html;
 runuser -u www-data -- wp core download;
 runuser -u www-data -- wp core config --dbname='wp' --dbuser='souf' --dbpass='souf123' --dbhost='mariadb' --dbprefix='wp_' --skip-check;
 runuser -u www-data -- wp core install --url='https://localhost' --title='Inception' --admin_user='sel-mars42' --admin_password='sel-mars42' --admin_email='sel-mars@1337.ma';
+runuser -u www-data -- wp plugin install redis-cache --activate;
+runuser -u www-data -- wp config set WP_REDIS_PORT 6379;
+runuser -u www-data -- wp config set WP_REDIS_HOST redis;
+runuser -u www-data -- wp redis enable;
 runuser -u www-data -- wp user create auth42 auth42@example.com --role=author --user_pass=auth42;
 sed -i -e "s/listen =.*/listen = 9000/" /etc/php/7.4/fpm/pool.d/www.conf;
 service php7.4-fpm start;
