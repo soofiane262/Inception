ef9af220a37ed943cdab5fd8d7f25e4ad2773052
Soufiane El marsi <sel-mars@e2r5p7.1337.ma>
2023-01-02 18:34:39 +0100

    ready

--- srcs/requirements/bonus/grafana/tools/grafana.sh
+++ srcs/requirements/bonus/grafana/tools/grafana.sh
@@ -1,4 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    grafana.sh                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: sel-mars <sel-mars@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/01/02 16:09:33 by sel-mars          #+#    #+#              #
+#    Updated: 2023/01/02 16:09:50 by sel-mars         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 #! /bin/bash
 cp /tmp/defaults.ini $GF_PATHS_CONFIG;
-grafana-cli grafana-cli admin reset-admin-password $GF_ADMIN_PSWD;
 grafana-server -config $GF_PATHS_CONFIG -homepath $GF_PATHS_HOME cfg:default.paths.logs=$GF_PATHS_LOGS;
