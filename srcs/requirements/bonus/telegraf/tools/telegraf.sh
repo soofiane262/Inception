94545e7f9da2f02eacf5dd408bc1937a652f4647
Soufiane El marsi <sel-mars@e2r4p10.1337.ma>
2023-01-07 21:56:14 +0100

    :(

--- srcs/requirements/bonus/telegraf/tools/telegraf.sh
+++ srcs/requirements/bonus/telegraf/tools/telegraf.sh
@@ -1,19 +1,17 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    website.sh                                         :+:      :+:    :+:    #
+#    telegraf.sh                                        :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: sel-mars <sel-mars@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/02 16:09:33 by sel-mars          #+#    #+#              #
-#    Updated: 2023/01/07 21:18:39 by sel-mars         ###   ########.fr        #
+#    Updated: 2023/01/07 21:46:46 by sel-mars         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #! /bin/bash
-tail -f
 if [ ! -e /etc/telegraf/telegraf.conf ]; then
-	apt-get install -y wget curl gnupg2 lsb-release ca-certificates apt-transport-https software-properties-common procps
 	curl -Lk https://repos.influxdata.com/influxdb.key | apt-key add -
 	echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" >> /etc/apt/sources.list.d/influxdb.list
 	apt-get update -y
