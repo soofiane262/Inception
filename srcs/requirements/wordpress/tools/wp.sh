31bd53bc9b76c331d1e9c0817658a6cf12bfbc61
Soufiane El marsi <sel-mars@e2r4p10.1337.ma>
2023-01-10 12:52:32 +0100

    wp-slight-change

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -6,7 +6,7 @@
 #    By: sel-mars <sel-mars@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/12/15 18:33:51 by sel-mars          #+#    #+#              #
-#    Updated: 2023/01/10 10:56:45 by sel-mars         ###   ########.fr        #
+#    Updated: 2023/01/10 12:51:40 by sel-mars         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,14 +15,14 @@ if [ ! -e /var/www/html/wp-config.php ]; then
 	echo -e "\n\e[3m\e[1;37m# ------------------------- Configuring Wordpress 1/2 ------------------------ #\e[0m\n"
 	cd /var/www/html
 	chown -R www-data:www-data /var/www/html
-	wp core download --allow-root
-	wp core config --dbname="$MYSQL_WP_DBNAME" --dbuser="$MYSQL_USERNAME" --dbpass="$MYSQL_PASSWORD" --dbhost='mariadb' --dbprefix='wp_' --skip-check --allow-root
-	wp core install --url="https://$DOMAIN_NAME" --title='Inception' --admin_user="$WP_ADMIN_USERNAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
-	wp plugin install redis-cache --activate --allow-root
-	wp config set WP_REDIS_PORT 6379 --allow-root
-	wp config set WP_REDIS_HOST redis --allow-root
-	wp redis enable --allow-root
-	wp user create $WP_AUTHOR_USERNAME $WP_AUTHOR_EMAIL --role=author --user_pass=$WP_AUTHOR_PASSWORD --allow-root
+	runuser -u www-data -- wp core download
+	runuser -u www-data -- wp core config --dbname="$MYSQL_WP_DBNAME" --dbuser="$MYSQL_USERNAME" --dbpass="$MYSQL_PASSWORD" --dbhost='mariadb' --dbprefix='wp_' --skip-check
+	runuser -u www-data -- wp core install --url="https://$DOMAIN_NAME" --title='Inception' --admin_user="$WP_ADMIN_USERNAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL"
+	runuser -u www-data -- wp plugin install redis-cache --activate
+	runuser -u www-data -- wp config set WP_REDIS_PORT 6379
+	runuser -u www-data -- wp config set WP_REDIS_HOST redis
+	runuser -u www-data -- wp redis enable
+	runuser -u www-data -- wp user create $WP_AUTHOR_USERNAME $WP_AUTHOR_EMAIL --role=author --user_pass=$WP_AUTHOR_PASSWORD
 fi
 echo -e "\n\e[3m\e[1;37m# ------------------------- Configuring Wordpress 2/2 ------------------------ #\e[0m\n"
 sed -i -e "s/listen =.*/listen = 9000/" /etc/php/$(wp --info | grep php.ini | cut -d'/' -f4 )/fpm/pool.d/www.conf
