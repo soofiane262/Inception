f9d9b63b70fb80d670ddb3a543963e0af35d369a
Soufiane El marsi <sel-mars@e2r4p10.1337.ma>
2023-01-10 14:26:22 +0100

    hope-wp-will-be-ok

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -6,7 +6,7 @@
 #    By: sel-mars <sel-mars@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/12/15 18:33:51 by sel-mars          #+#    #+#              #
-#    Updated: 2023/01/10 13:39:18 by sel-mars         ###   ########.fr        #
+#    Updated: 2023/01/10 14:26:04 by sel-mars         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,14 +14,14 @@
 if [ ! -e /var/www/html/wp-config.php ]; then
 	echo -e "\n\e[3m\e[1;37m# ------------------------- Configuring Wordpress 1/2 ------------------------ #\e[0m\n"
 	chown -R www-data:www-data /var/www/html
-	runuser -u www-data -- wp core download --path=/var/www/html
-	runuser -u www-data -- wp core config --dbname="$MYSQL_WP_DBNAME" --dbuser="$MYSQL_USERNAME" --dbpass="$MYSQL_PASSWORD" --dbhost='mariadb' --dbprefix='wp_' --skip-check --path=/var/www/html
-	runuser -u www-data -- wp core install --url="https://$DOMAIN_NAME" --title='Inception' --admin_user="$WP_ADMIN_USERNAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path=/var/www/html
-	runuser -u www-data -- wp plugin install redis-cache --activate --path=/var/www/html
-	runuser -u www-data -- wp config set WP_REDIS_PORT 6379 --path=/var/www/html
-	runuser -u www-data -- wp config set WP_REDIS_HOST redis --path=/var/www/html
-	runuser -u www-data -- wp redis enable --path=/var/www/html
-	runuser -u www-data -- wp user create $WP_AUTHOR_USERNAME $WP_AUTHOR_EMAIL --role=author --user_pass=$WP_AUTHOR_PASSWORD --path=/var/www/html
+	runuser -u www-data -- wp core download --path="/var/www/html"
+	runuser -u www-data -- wp core config --dbname="$MYSQL_WP_DBNAME" --dbuser="$MYSQL_USERNAME" --dbpass="$MYSQL_PASSWORD" --dbhost='mariadb' --dbprefix='wp_' --skip-check --path="/var/www/html"
+	runuser -u www-data -- wp core install --url="https://$DOMAIN_NAME" --title='Inception' --admin_user="$WP_ADMIN_USERNAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path="/var/www/html"
+	runuser -u www-data -- wp plugin install redis-cache --activate --path="/var/www/html"
+	runuser -u www-data -- wp config set WP_REDIS_PORT 6379 --path="/var/www/html"
+	runuser -u www-data -- wp config set WP_REDIS_HOST redis --path="/var/www/html"
+	runuser -u www-data -- wp redis enable --path="/var/www/html"
+	runuser -u www-data -- wp user create $WP_AUTHOR_USERNAME $WP_AUTHOR_EMAIL --role=author --user_pass=$WP_AUTHOR_PASSWORD --path="/var/www/html"
 fi
 echo -e "\n\e[3m\e[1;37m# ------------------------- Configuring Wordpress 2/2 ------------------------ #\e[0m\n"
 sed -i -e "s/listen =.*/listen = 9000/" /etc/php/$(wp --info | grep php.ini | cut -d'/' -f4 )/fpm/pool.d/www.conf
